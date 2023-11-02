bec1d4e88f2a4b7a4519e296b7c42a38db0ff52b
Yokiebob <126304146+Yokiebob@users.noreply.github.com>
2023-11-02 17:53:39 -0500

    Update fancyLinks.js for the last time please

--- fancyLinks.js
+++ fancyLinks.js
@@ -3,7 +3,7 @@ for(var i = 0; i < elements_with_flag.length; i++){
 	const curr = elements_with_flag[i];
 	
 	var currText = curr.innerHTML;
-	currText.split("/");
+	currText = currText.split("/");
 	currText.splice(0,1);
 	for(var k = 0; k < currText.length; k++){
 		const gotoLink = currText.slice(0,k + 1).join("/");
