03675898b3486756bb72870bb4ebdf9ad32e3cd2
Yokiebob <126304146+Yokiebob@users.noreply.github.com>
2023-11-02 18:10:18 -0500

    Update fancyLinks.js FOR THE LAST TIME NOW FOR REAL

--- fancyLinks.js
+++ fancyLinks.js
@@ -4,12 +4,11 @@ for(var i = 0; i < elements_with_flag.length; i++){
 	
 	var currText = curr.innerHTML;
 	currText = currText.split("/");
-	for(var k = 0; k < currText.length; k++){
-		const gotoLink = currText.slice(0,k + 1).join("/");
-		currText[k] = `<a href = ${"https://yokiebob.github.io/" + gotoLink.toLowerCase=="yokiebob"?"":gotoLink}> ${currText[k]} </a>`;
-	}
 	curr.innerHTML = "";
+	
 	for(var k = 0; k < currText.length; k++){
-		curr.innerHTML += currText[k];
+		const gotoLink = currText.slice(0,k + 1).join("/").replace("Yokiebob/","");
+		console.log(currText[k]);
+		curr.innerHTML += `<a href = "${"https://yokiebob.github.io/" + (currText[k] == "Yokiebob" ? "" : gotoLink)}"> ${currText[k]} </a>`;
 	}
 }
