22bb0fd0f0e0327df0de7ed7600fc1fa0ea8fb5d
Yokiebob <126304146+Yokiebob@users.noreply.github.com>
2023-11-02 17:51:55 -0500

    Update fancyLinks.js

--- fancyLinks.js
+++ fancyLinks.js
@@ -3,11 +3,14 @@ for(var i = 0; i < elements_with_flag.length; i++){
 	const curr = elements_with_flag[i];
 	
 	var currText = curr.innerHTML;
-	currText = currText.split("/");
+	currText.split("/");
 	currText.splice(0,1);
 	for(var k = 0; k < currText.length; k++){
 		const gotoLink = currText.slice(0,k + 1).join("/");
 		currText[k] = `<a href = ${"https://yokiebob.github.io/" + gotoLink}></a>`;
 	}
-	curr.innerHTML = currText.join("");
+	curr.innerHTML = "";
+	for(var k = 0; k < currText.length; k++){
+		curr.innerHTML += currText[k];
+	}
 }
