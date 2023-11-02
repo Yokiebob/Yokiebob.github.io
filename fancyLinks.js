0096e5ac61e74d622458c362f4b9de6d6594b60a
Yokiebob <126304146+Yokiebob@users.noreply.github.com>
2023-11-02 17:33:18 -0500

    Add files via upload

---
+++ fancyLinks.js
@@ -0,0 +1,12 @@
+const elements_with_flag = document.getElementsByClassName('niceLinkFormat');
+for(var i = 0; i < elements_with_flag.length; i++){
+	const curr = elements_with_flag[i];
+	
+	var currText = curr.innerHTML;
+	currText.split("/");
+	currText.splice(0,1);
+	for(var k = 0; k < currText.length; k++){
+		const gotoLink = currText.slice(0,k + 1).join("/");
+		currText[k] = `<a href = ${"https://yokiebob.github.io/" + gotoLink}></a>`;
+	}
+}
